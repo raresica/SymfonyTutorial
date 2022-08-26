7340e7df7f1e8c001c00f4074ca3cb5cd171fdb5
raresonescu <raresonescu@gmail.com>
2022-08-26 11:11:52 +0300

    Chapter 4: create a new page and set the url

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
@@ -11,5 +12,18 @@ class VinylController
     public function homepage():Response
     {
        return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null):Response
+    {
+        if($slug){
+            $title ='Genre:'.u(str_replace('-',' ', $slug))->title(true);
+
+        }else{
+            $title= 'All Genres';
+        }
+
+        return new Response('Scriu ceva: '.$title);
     }
 }
