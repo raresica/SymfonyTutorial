993578c7cc71ce53d9f53ffa263c33654acae425
raresonescu <raresonescu@gmail.com>
2022-08-26 14:00:31 +0300

    Chapter 7: display an array of music in twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,28 +2,42 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
-    public function homepage():Response
+    public function homepage(): Response
     {
-       return new Response('Title: PB and Jams');
+
+        $tracks = [
+            ['song' => 'Ryan Star', 'artist' => 'Brand New Day'],
+            ['song' => 'Linkin Park', 'artist' => 'CASTLE OF GLASS'],
+            ['song' => 'T-pain', 'artist' => 'Wake up Dead'],
+            ['song' => 'Joyner Lucas', 'artist' => 'I Don\'t Die'],
+            ['song' => 'Eminem', 'artist' => 'Lucky you'],
+        ];
+
+
+        return $this->render('vinyl/homepage.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
-    public function browse(string $slug = null):Response
+    public function browse(string $slug = null): Response
     {
-        if($slug){
-            $title ='Genre:'.u(str_replace('-',' ', $slug))->title(true);
+        if ($slug) {
+            $title = 'Genre:' . u(str_replace('-', ' ', $slug))->title(true);
 
-        }else{
-            $title= 'All Genres';
+        } else {
+            $title = 'All Genres';
         }
 
-        return new Response('Scriu ceva: '.$title);
+        return new Response('Scriu ceva: ' . $title);
     }
 }
