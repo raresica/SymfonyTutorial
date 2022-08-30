d67bb5964b2690304019403f772626e443500ff3
raresonescu <raresonescu@gmail.com>
2022-08-30 11:57:50 +0300

    generate urls

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -9,7 +9,7 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
 
@@ -28,16 +28,12 @@ class VinylController extends AbstractController
         ]);
     }
 
-    #[Route('/browse/{slug}')]
+    #[Route('/browse/{slug}', name:'app_browse')]
     public function browse(string $slug = null): Response
     {
-        if ($slug) {
-            $title = 'Genre:' . u(str_replace('-', ' ', $slug))->title(true);
-
-        } else {
-            $title = 'All Genres';
-        }
-
-        return new Response('Scriu ceva: ' . $title);
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        return $this->render('vinyl/browse.html.twig',[
+            'genre' => $genre,
+        ]);
     }
 }
